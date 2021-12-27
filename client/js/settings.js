8aa739d38968560c8b5835a697352a1830e9619b
un5450 <un5450@outlook.com>
2021-12-28 07:27:58 +0900

    Client: update settings.js

--- client/js/settings.js
+++ client/js/settings.js
@@ -1,7 +1,34 @@
 HDT.getCurrentUser().then(user => {
+    if (!user) {
+        window.location = "/";
+        return;
+    }
     $("#input-email").val(user.email);
     $("#input-name").val(user.name);
     $("#input-meta").val(JSON.stringify(user.meta));
+})
+
+$("#button-submit").on("click", async() => {
+    let user = await HDT.getCurrentUser();
+    let id = user._id
+    let name = $("#input-name").val() || undefined;
+    let meta = $("#input-meta").val() || undefined;
+
+    $A.patch(`/api/users/${id}`, {
+        body: {
+            "name": name,
+            "meta": meta
+        }
+    }).then(async res => {
+        if (!res.ok) {
+            let err = await res.json().error;
+            alert("업데이트 실패: " + err);
+            return;
+        } else {
+            alert("적용되었습니다.");
+            window.location.reload();
+        }
+    })
 })
 
 HDT.fileLoaded("projectlist.js");
